4b55f3a6ce2be1dbcedd8fed72f8044780adb05a
MVPSuk <102248521+MVPSuk@users.noreply.github.com>
2022-11-04 20:30:04 +0000

    Commit 04-11-2022 17:29

--- conflicts/scripts.js
+++ conflicts/scripts.js
@@ -1,22 +1,41 @@
 function abrirNav(){
-    document.getElementById('nav').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById('nav').style.width = '18vw';
     document.getElementById('backscreen').style.width = '100%';
 }
 
 function fecharNav(){
     document.getElementById('nav').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
     document.getElementById('backscreen').style.width = '0';
 }
 
-function abrirCList(){
-    var cLOpen = document.getElementById('conflictlist').style.height;
-    if(cLOpen=='0px'){
-        document.getElementById('conflictlist').style.height = '170px';
-        document.getElementById('point1').style.transform = 'rotate(90deg)'
-    }else{
-        document.getElementById('conflictlist').style.height = '0px';
-        document.getElementById('point1').style.transform = 'rotate(0deg)'
+function abrirNav2(continente) {
+    abrirNav();
+
+    var main = document.getElementById('main');
+    lista = continente.getAttribute('data-lists-names').split(',');
+    links = continente.getAttribute('data-lists-links').split(',');
+    all = '';
+    for (i = 0; i < lista.length; i++) {
+        all += '<li><a href="' + links[i] + '">' + lista[i] + '</a></li>';
+    }
+
+    document.getElementById('title').innerHTML = "Conflitos<br><b>" + continente.getAttribute('continente') + "</b>";
+    document.getElementById('nav2-1').innerHTML = '<ul>' + all + '</ul>';
+    document.getElementById('nav2').style.width = '18vw';
+    document.getElementById('conflictlist').style.height = '170px';
+    document.getElementById('pointconflictlist').style.transform = 'rotate(90deg)';
+    main.style.paddingRight = '18vw';
+}
+
+function abrirCList() {
+    var cLOpen = document.getElementById('conflictlist');
+    var seta = document.getElementById('pointconflictlist');
+
+    if (cLOpen.style.height != '170px') {
+        cLOpen.style.height = '170px';
+        seta.style.transform = 'rotate(90deg)';
+    } else if (cLOpen.style.height != '0px') {
+        cLOpen.style.height = '0px';
+        seta.style.transform = 'rotate(0deg)';
     }
 }
